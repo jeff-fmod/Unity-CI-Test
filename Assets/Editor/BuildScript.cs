e37baf94171efe7bb02a46500f6893187f6d2093
Jeff Dunn <info@audimancy.com>
2024-05-07 11:52:41 +1000

    Added build script

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+public class BuildScript : MonoBehaviour
+{
+    public static void Build()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
+        options.target = EditorUserBuildSettings.activeBuildTarget;
+        options.locationPathName = "C:\\dev\\MyProject.exe";
+        options.targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+        BuildPipeline.BuildPlayer(options);
+    }
+}
+#endif
