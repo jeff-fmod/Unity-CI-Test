4064395c4086b638ff8668f388cc58094f0e738d
Jeff Dunn <info@audimancy.com>
2024-05-09 09:59:59 +1000

    Build script changes

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -7,12 +7,12 @@ public class BuildScript : MonoBehaviour
     {
         BuildPlayerOptions options = new BuildPlayerOptions();
         options.scenes = new string[] { "Assets/Scenes/SampleScene.unity" };
-        options.target = EditorUserBuildSettings.activeBuildTarget;
+        options.target = BuildTarget.StandaloneWindows64;
         options.locationPathName = "C:\\Users\\Public\\Documents\\Unity-CI-Test\\_builds\\MyProject.exe";
         options.targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
         options.options = BuildOptions.Development | BuildOptions.AllowDebugging;
 
-        FMODUnity.EventManager.CopyToStreamingAssets(BuildTarget.StandaloneWindows64);
+        //FMODUnity.EventManager.CopyToStreamingAssets(BuildTarget.StandaloneWindows64);
 
         BuildPipeline.BuildPlayer(options);
     }
